f2a0f72bcf6bf1fc8fcef34c8727a411630aa133
cry1s <cry1s@ya.ru>
2024-01-12 07:06:21 +0300

    ProjectUser pivot

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Project extends Model
 {
@@ -25,5 +26,10 @@ class Project extends Model
 
     public function importance() : BelongsTo {
         return $this->belongsTo(Importance::class);
+    }
+
+    public function users(): HasManyThrough
+    {
+        return $this->hasManyThrough(User::class, ProjectUser::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -62,5 +63,10 @@ class User extends Authenticatable
 
     public function ratingLogs() : HasMany {
         return $this->hasMany(RatingLog::class);
+    }
+
+    public function projects(): HasManyThrough
+    {
+        return $this->hasManyThrough(Project::class, ProjectUser::class);
     }
 }
