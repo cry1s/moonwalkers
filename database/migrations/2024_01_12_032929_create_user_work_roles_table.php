8997106ca7ba8dd41a463deabdecd4405753b06f
cry1s <cry1s@ya.ru>
2024-01-12 06:47:33 +0300

    unique

--- database/migrations/2024_01_12_032929_create_user_work_roles_table.php
+++ database/migrations/2024_01_12_032929_create_user_work_roles_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->foreignIdFor(User::class);
             $table->enum('role', ['tutor', 'techie', 'actor', 'releaser', 'designer']);
             $table->timestamps();
+
+            $table->unique('role', 'user_id');
         });
     }
 
